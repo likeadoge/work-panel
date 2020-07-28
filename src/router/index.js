1f2e1c13dbf81c3821f910543aaaf1a5a27f0e8e
likeadoge <62692265+likeadoge@users.noreply.github.com>
2020-07-28 19:11:53 +0800

    修复 router 报错

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Project from '../views/Project.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +14,11 @@ Vue.use(VueRouter)
   {
     path: '/project',
     name: 'Project',
+    component: Project
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Project.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/Project.vue')
   }
 ]
 
