8f72df1caf5484771022a169baec9e47bd08c729
zhangqiaoli <zhangqiaoli@dameng.com>
2020-07-30 18:30:26 +0800

    新增创建项目modal

---
+++ src/views/components/CreateProjectModal.vue
@@ -0,0 +1,75 @@
+<template>
+  <a-modal :visible= "visibleProject" title="新建项目" @cancel="()=> (this.visibleProject = false)" @ok="onSubmit" cancelText="取消" okText="确定" :width="800" centered>
+    <a-form-model :model="form" :rules="rules" ref="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+      <a-form-model-item label="项目名称" ref="name" prop="name">
+        <a-input v-model="form.name" placeholder="请填写项目名称"/>
+      </a-form-model-item>
+      <a-form-model-item label="项目模板" ref="template" prop="template">
+        <a-select v-model="form.template" placeholder="请选择项目模板">
+          <a-select-option value="shanghai">
+            空白模板
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item label="所属部门">
+        <a-input v-model="form.depart" value="达梦数据库" placeholder="达梦数据库" disabled/>
+      </a-form-model-item>    
+    <a-form-model-item label="起止时间">
+      <a-range-picker style="width: 100%;"
+      :ranges="{ '今天': [moment(), moment()], '本月': [moment(), moment().endOf('month')] }"
+      @change="onChange"
+    />
+    </a-form-model-item>
+    <a-form-model-item label="项目描述">
+      <a-input v-model="form.desc" type="textarea" />
+    </a-form-model-item>
+  </a-form-model>
+  </a-modal>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  data: () => ({
+    visibleProject: false,
+    labelCol: { span: 4 },
+    wrapperCol: { span: 14 },
+      form: {
+        name: '',
+        template: '',
+        depart: "达梦数据库",
+        date1: undefined,
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: '',
+      },
+      rules: {
+        name: [
+          {required: true, message: '请输入项目名称', trigger: "change"}
+        ],
+        template: [
+          {required: true, message: '请选择项目模板', trigger: "change"}
+        ]
+      }
+  }),
+  methods: {
+    moment,
+    onSubmit() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+
+  },
+
+}
+
+</script>
+<style scoped>
+</style>
