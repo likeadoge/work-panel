dc272add5dc76cd93fb53c2172478aaf603edfd4
zhangqiaoli <zhangqiaoli@dameng.com>
2020-08-01 17:13:31 +0800

    补充home页侧边栏内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,17 +13,21 @@
             <span slot="title">
               <icon-font type="icon-projectCount" class="sidebarfont" />项目管理
             </span>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
+            <template v-for="(v,i) in projectList" >
+              <a-menu-item :key="i">{{v.name}}</a-menu-item>
+            </template>
+            
+            <!-- <a-menu-item key="2">option2</a-menu-item>
             <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
+            <a-menu-item key="4">option4</a-menu-item> -->
             <a-menu-item key="0">
               <a-button type="link" style="padding: 0px;" @click="showProject"><a-icon style="margin-right: 0px;" type="plus"/>新建项目</a-button>
             </a-menu-item>
             
           </a-sub-menu>
           <a-menu-item key="sub2">
-            <icon-font type="icon-template" class="sidebarfont"/>模板
+            <!-- <icon-font type="icon-template" class="sidebarfont"/>模板 -->
+            <router-link to="/template"><icon-font type="icon-template" class="sidebarfont"/>模板库</router-link>
           </a-menu-item>
           <a-menu-item key="sub3">
             <icon-font type="icon-library" class="sidebarfont"/>归档库
@@ -40,15 +44,23 @@
         </a-menu>
       </a-layout-sider>
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px', backgroundColor: '#fcfcfd' }">
-      
         <task-panel
+          v-for="(v) in projectList"
+          :key="v.id"
+          :title="v.name"
+          :sub-title="v.describe"
+          :boards="v.template"
+          @sort="boards=>sortBoards(v.id,boards)"
+        />
+      
+        <!-- <task-panel
           v-for="(v) in list"
           :key="v.id"
           :title="v.title"
           :sub-title="v.subTitle"
           :boards="v.boards"
           @sort="boards=>sortBoards(v.id,boards)"
-        />
+        /> -->
         <create-project-modal ref="project"/>
       </a-layout-content>
     </a-layout>
@@ -57,9 +69,12 @@
 
 <script>
 import TaskPanel from "./home/TaskPanel";
-import ph from "@/utils/placeholder";
-import uuid from "@/utils/uuid";
+// import ph from "@/utils/placeholder";
+// import uuid from "@/utils/uuid";
 import CreateProjectModal from "./components/CreateProjectModal";
+// import { mapGetters, mapMutations } from "vuex";
+import * as item from '../request/item'
+// import * as item from "@/request/item";
 
 export default {
   components: {
@@ -67,15 +82,17 @@ export default {
   },
   data: () => ({
     visibleProject: false,
+    status: '1',
     // list:[]
-    list: new Array(5).fill(0).map(() => ({
-      id: uuid(), // 项目 id
-      title: ph(), // 标题
-      subTitle: ph(), // 副标题
-      boards: new Array(5).fill(0).map(() => ({
-        title: ph(), // 标题
-      })),
-    })),
+    // list: new Array(5).fill(0).map(() => ({
+    //   id: uuid(), // 项目 id
+    //   title: ph(), // 标题
+    //   subTitle: ph(), // 副标题
+    //   boards: new Array(5).fill(0).map(() => ({
+    //     title: ph(), // 标题
+    //   })),
+    // })),
+    projectList: []
   }),
   methods:{
     sortBoards(id,arr){
@@ -85,9 +102,21 @@ export default {
     showProject() {
       this.$refs.project.visibleProject = true
     },
+    // getInfo() {
+    //   item.getProject().then((res) => {
+    //     console.log(res)
+    //     this.projectList = res
+    //     console.log(this.projectList)
+    //   })
+    // }
   },
 
+
   mounted() {
+    item.getProject().then((res) => {
+        console.log(res)
+        this.projectList = res
+    })
   },
 };
 </script>
@@ -112,5 +141,8 @@ body {
 .sidebarfont {
   font-size: 14px;
   color: #aaa;
+}
+.ant-menu >>> ul {
+  background-color: #fcfcfd;
 }
 </style>
