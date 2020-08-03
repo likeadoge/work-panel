e2a27ffb34a5cd816cb5b563e7fc4bdf2e96a61f
zhangqiaoli <zhangqiaoli@dameng.com>
2020-08-03 09:25:42 +0800

    提交归档库

---
+++ src/views/Library.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="padding: 24px;">
+    nihao
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+ data() {
+   return {
+   }
+ },
+}
+
+</script>
+<style scoped>
+</style>
