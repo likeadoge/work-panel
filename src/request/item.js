5b79bd14707b19034491721fba515c7752a60f75
zhangqiaoli <zhangqiaoli@dameng.com>
2020-08-03 19:10:33 +0800

    补充获取模板、归档库等接口

--- src/request/item.js
+++ src/request/item.js
@@ -4,29 +4,40 @@ import { post, get } from '@/request/http'
 // import Vue from 'vue'
 
 export const addProject = ({
-    id,
     name,
     template,
-    orgld,
+    orgId,
     beginTime,
     endTime,
     describe
 }) => post('/task/project/add', {
-    id,
     name,
     template,
-    orgld,
+    orgId,
     beginTime,
     endTime,
     describe
 })
 
-export const getProject = () => get('/task/project/get', { status: 1 }).then((res) => {
+export const getProject = (status) => get('/task/project/get', { status: status }).then((res) => {
     return res
 })
 
 export const getDepart = () => get('/sys/sysDepart/queryList', {}).then((res) => {
     return res
 })
+
+export const getTemplate = () => get('/task/project/getTemplate', {}).then((res) => {
+    return res
+}).catch((err) => {
+    console.log(err)
+})
+
+
+export const getBoard = (id) => get('/task/panel/get', { projectId: id }).then((res) => {
+    return res
+})
+
+export const createBoard = ({ name, projectId }) => post('/task/panel/create', { name, projectId: projectId })
 
 // export const getUserInfo = () => get.withoutError('/sys/userInfo', {})
