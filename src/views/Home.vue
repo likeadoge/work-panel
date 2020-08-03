f8d6b7b7b8e8a036be18b2a217ace68506a4bdb2
zhangqiaoli <zhangqiaoli@dameng.com>
2020-08-03 19:11:54 +0800

    调整页面布局为左右布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,59 +1,86 @@
 <template>
   <div style="height:100%;padding: 32px 48px;">
     <a-layout style="height:100%;padding: 24px 0; background-color: #fcfcfd;">
-      <a-layout-sider width="216px" style="overflow:auto; ">
-        <a-menu
+      <a-layout-sider width="216px" class="bg" style="overflow:auto;">
+        <!-- <a-menu
           mode="inline"
           theme="light"
           :default-selected-keys="['1']"
           :default-open-keys="['sub1']"
           style="height: 100%; background-color: #fcfcfd;"
-        >
+            >
           <a-sub-menu key="sub1">
             <span slot="title">
               <icon-font type="icon-projectCount" class="sidebarfont" />项目管理
             </span>
-            <template v-for="(v,i) in projectList" >
+            <template v-for="(v,i) in projectList">
               <a-menu-item :key="i">{{v.name}}</a-menu-item>
             </template>
-            
-            <!-- <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item> -->
             <a-menu-item key="0">
-              <a-button type="link" style="padding: 0px;" @click="showProject"><a-icon style="margin-right: 0px;" type="plus"/>新建项目</a-button>
+              <a-button type="link" style="padding: 0px;" @click="showProject">
+                <a-icon style="margin-right: 0px;" type="plus" />新建项目
+              </a-button>
             </a-menu-item>
-            
           </a-sub-menu>
           <a-menu-item key="sub2">
-            <!-- <icon-font type="icon-template" class="sidebarfont"/>模板 -->
-            <router-link to="/template"><icon-font type="icon-template" class="sidebarfont"/>模板库</router-link>
+            <router-link to="/template">
+              <icon-font type="icon-template" class="sidebarfont" />模板库
+            </router-link>
           </a-menu-item>
           <a-menu-item key="sub3">
-            <!-- <icon-font type="icon-library" class="sidebarfont"/>归档库 -->
-            <router-link to="/library"><icon-font type="icon-library" class="sidebarfont"/>归档库</router-link>
+            <router-link to="/library">
+              <icon-font type="icon-library" class="sidebarfont" />归档库
+            </router-link>
           </a-menu-item>
-          <!-- <a-sub-menu key="sub3">
+        </a-menu> -->
+<!-- :default-open-keys="['sub1']" -->
+        <a-menu
+          mode="inline"
+          theme="light"
+          :default-open-keys="['sub1']"
+          style="height: 100%; background-color: #fcfcfd;"
+        >    
+        <a-sub-menu key="sub1" @click="handleSelected1">
             <span slot="title">
-              <icon-font type="icon-library" />已归档
+              <icon-font type="icon-projectCount" class="sidebarfont" />项目管理
             </span>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu> -->
+            <template v-for="(v,i) in projectList">
+              <a-menu-item :key="i">{{v.name}}</a-menu-item>
+            </template>
+            <a-menu-item key="0">
+              <a-button type="link" style="padding: 0px;" @click="showProject">
+                <a-icon style="margin-right: 0px;" type="plus" />新建项目
+              </a-button>
+            </a-menu-item>
+          </a-sub-menu>
+          <a-menu-item key="sub2" @click="handleSelected2">
+            <!-- <router-link to="/template"> -->
+              <icon-font type="icon-template" class="sidebarfont" />模板库
+            <!-- </router-link> -->
+          </a-menu-item>
+          <a-menu-item key="sub3" @click="handleSelected3">
+            <!-- <router-link to="/library"> -->
+              <icon-font type="icon-library" class="sidebarfont" />归档库
+            <!-- </router-link> -->
+          </a-menu-item>
         </a-menu>
       </a-layout-sider>
-      <a-layout-content :style="{ padding: '0 24px', minHeight: '280px', backgroundColor: '#fcfcfd' }">
+      <a-layout-content
+        :style="{ padding: '0 24px', minHeight: '280px', backgroundColor: '#fcfcfd' }"
+      >
+     
         <task-panel
           v-for="(v) in projectList"
           :key="v.id"
+          :id="v.id"
           :title="v.name"
           :sub-title="v.describe"
-          :boards="v.template"
-          @sort="boards=>sortBoards(v.id,boards)"
+          v-show="menuChange == 'sub1'"
+          @sort="boards=>sortBoards(v.id, boards)"
         />
-      
+        <project-template v-show="menuChange == 'sub2'"/>
+        <library v-show="menuChange == 'sub3'"/>
+        <!-- :boards="v.template" -->
         <!-- <task-panel
           v-for="(v) in list"
           :key="v.id"
@@ -61,8 +88,11 @@
           :sub-title="v.subTitle"
           :boards="v.boards"
           @sort="boards=>sortBoards(v.id,boards)"
-        /> -->
-        <create-project-modal ref="project"/>
+        />-->
+       <create-project-modal ref="project" @addProject="addProject" />
+      
+        
+        <route-view></route-view>
       </a-layout-content>
     </a-layout>
   </div>
@@ -70,20 +100,30 @@
 
 <script>
 import TaskPanel from "./home/TaskPanel";
+import CreateProjectModal from "./components/CreateProjectModal";
+import * as item from "../request/item";
+import ProjectTemplate from "./ProjectTemplate"
+import Library from "./Library"
 // import ph from "@/utils/placeholder";
 // import uuid from "@/utils/uuid";
-import CreateProjectModal from "./components/CreateProjectModal";
 // import { mapGetters, mapMutations } from "vuex";
-import * as item from '../request/item'
 // import * as item from "@/request/item";
 
 export default {
   components: {
-    TaskPanel, CreateProjectModal,
+    TaskPanel,
+    CreateProjectModal,
+    ProjectTemplate,
+    Library
   },
   data: () => ({
     visibleProject: false,
-    status: '1',
+    status: "1",
+    menuChange: 'sub1',
+    projectList: [],
+    current: ['2'],
+    defaultSelectedKeys: [],
+    openKeys: []
     // list:[]
     // list: new Array(5).fill(0).map(() => ({
     //   id: uuid(), // 项目 id
@@ -93,16 +133,57 @@ export default {
     //     title: ph(), // 标题
     //   })),
     // })),
-    projectList: []
   }),
-  methods:{
-    sortBoards(id,arr){
-      if(this.list.find(v=>v.id == id))
-      this.list.find(v=>v.id == id).boards = arr
+  methods: {
+    loadProject() {
+      item.getProject().then(res => {
+        console.log(res);
+        this.projectList = res;
+      });
+    },
+    addProject(value) {
+      const { name, template, orgId, beginTime, endTime, describe } = value;
+      item
+        .addProject({
+          name,
+          template,
+          orgId,
+          beginTime,
+          endTime,
+          describe
+        })
+        .then(() => {
+          this.loadProject()
+        })
+        .catch(msg => this.message(msg));
+    },
+    sortBoards(id, arr) {
+      if (this.list.find(v => v.id == id))
+        this.list.find(v => v.id == id).boards = arr;
     },
     showProject() {
-      this.$refs.project.visibleProject = true
+      this.$refs.project.visibleProject = true;
     },
+    // onOpenChange (openKeys) {
+    //   console.log(openKeys)
+    //     this.openKeys = openKeys
+    //   },
+    //   updateMenu () {
+    //     let routes = this.$route.matched.concat()
+    //     this.defaultSelectedKeys = [ routes.pop().path ]
+    //   },
+      handleSelected1(){
+        console.log("点击了项目管理")
+        this.menuChange = 'sub1'
+      },
+      handleSelected2(){
+        console.log("点击了模板库")
+        this.menuChange = 'sub2'
+      },
+      handleSelected3(){
+        console.log("点击了归档库")
+        this.menuChange = 'sub3'
+      }
     // getInfo() {
     //   item.getProject().then((res) => {
     //     console.log(res)
@@ -112,13 +193,9 @@ export default {
     // }
   },
 
-
   mounted() {
-    item.getProject().then((res) => {
-        console.log(res)
-        this.projectList = res
-    })
-  },
+    this.loadProject();
+  }
 };
 </script>
 
@@ -144,6 +221,9 @@ body {
   color: #aaa;
 }
 .ant-menu >>> ul {
+  background-color: #fcfcfd;
+}
+.bg {
   background-color: #fcfcfd;
 }
 </style>
